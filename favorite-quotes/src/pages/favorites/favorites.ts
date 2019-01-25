0e443d7263369451d7a7b314976c6f0c1ab48eb5
Unknown <opticalfalicy@gmail.com>
2019-01-25 04:24:06 -0600

    Up to lesson 57

--- favorite-quotes/src/pages/favorites/favorites.ts
+++ favorite-quotes/src/pages/favorites/favorites.ts
@@ -1,22 +1,17 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { Quote } from "../../data/quote.interface";
+import { QuotesService } from "../../services/quotes";
 
-/**
- * Generated class for the FavoritesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: "page-favorites",
   templateUrl: "favorites.html"
 })
 export class FavoritesPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  quotes: Quote[];
 
-  ionViewDidLoad() {
-    console.log("ionViewDidLoad FavoritesPage");
+  constructor(private quotesService: QuotesService) {}
+
+  ionViewWillEnter() {
+    this.quotes = this.quotesService.getFavoriteQuotes();
   }
 }
--- favorite-quotes/src/pages/quotes/quotes.ts
+++ favorite-quotes/src/pages/quotes/quotes.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  AlertController
+} from "ionic-angular";
 import { Quote } from "../../data/quote.interface";
+import { QuotesService } from "../../services/quotes";
 
 /**
  * Generated class for the QuotesPage page.
@@ -17,7 +23,11 @@ import { Quote } from "../../data/quote.interface";
 export class QuotesPage implements OnInit {
   quoteGroup: { category: string; quotes: Quote[]; icon: string };
 
-  constructor(private navParams: NavParams) {}
+  constructor(
+    private navParams: NavParams,
+    private alertCtrl: AlertController,
+    private quotesService: QuotesService
+  ) {}
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
@@ -28,4 +38,29 @@ export class QuotesPage implements OnInit {
   //
   //    add elvis operator (?) in html to work
   // }
+
+  onAddToFavorites(selectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: "Add Quote",
+      subTitle: "Are you sure?",
+      message: "Are you sure you want to add this quote?",
+      buttons: [
+        {
+          text: "For sure",
+          handler: () => {
+            this.quotesService.addQuoteToFavorites(selectedQuote);
+          }
+        },
+        {
+          text: "Nah",
+          role: "cancel",
+          handler: () => {
+            console.log("Cancelled");
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
 }
